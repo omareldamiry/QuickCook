c91647a5d3d0eeec5489edb1eab41f74dcd0b291
Omareldamiry <omar.eldamiry@gmail.com>
2021-08-10 09:17:01 +0300

    Experimental commit

---
+++ quickcook/lib/screens/RecipeDetails.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quickcook/RecipeHandler.dart';
+import 'package:quickcook/db_service.dart';
+
+class RecipeDetails extends StatelessWidget {
+  final String id;
+  final BuildContext context;
+  RecipeDetails(this.context, this.id){
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Future<Recipe> recipeInstance = context.read<RecipeDA>().getRecipe(id);
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          "Details",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}
