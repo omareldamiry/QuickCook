ef26da537a70b37a2eb21e78093fb0d7672120d1
Omareldamiry <omar.eldamiry@gmail.com>
2021-08-15 10:45:05 +0300

    source file renaming

---
+++ quickcook/lib/screens/MyRecipesPage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:quickcook/screens/AddRecipeForm.dart';
+import 'package:quickcook/utilities/RecipeHandler.dart';
+import 'package:quickcook/widgets/appbar.dart';
+import 'package:quickcook/widgets/drawer.dart';
+
+class MyRecipesPage extends StatefulWidget {
+  @override
+  _MyRecipesPage createState() => _MyRecipesPage();
+}
+
+class _MyRecipesPage extends State<MyRecipesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: myAppBar(title: "My Recipes"),
+      drawer: MyDrawer(currentRoute: '/myrecipes',),
+      body: Container(
+        alignment: Alignment.center,
+        child: MyRecipeList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddRecipePage()));
+        },
+      ),
+    );
+  }
+}
---
+++ quickcook/lib/screens/SearchPage.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:quickcook/models/Ingredient.dart';
+import 'package:quickcook/widgets/appbar.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPage createState() => _SearchPage();
+}
+
+class _SearchPage extends State<SearchPage> {
+  List<IngredientTile> ingredients = Ingredient.ingredientsList
+      .map((e) => IngredientTile(
+            title: e.toString(),
+          ))
+      .toList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: myAppBar(title: "Recipe search"),
+      body: Container(
+        child: ListView(
+          children: ingredients,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.arrow_forward,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+          Navigator.pushReplacementNamed(context, '/', arguments: ingredientQuery(ingredients));
+        },
+      ),
+    );
+  }
+
+  List<String>? ingredientQuery(List<IngredientTile> ingredients) {
+    List<String>? query = [];
+
+    ingredients.forEach((element) {
+      // ignore: unnecessary_statements
+      element.isTrue ? query!.add(element.title) : null;
+    });
+
+    if (query.isEmpty) {
+      query = null;
+    }
+
+    return query;
+  }
+}
+
+// ignore: must_be_immutable
+class IngredientTile extends StatefulWidget {
+  String title;
+  int? index;
+  bool isChecked;
+  IngredientTile(
+      {Key? key, required this.title, this.index, this.isChecked = false})
+      : super(key: key);
+
+  get isTrue {
+    return isChecked;
+  }
+
+  void newValue(bool newValue) {
+    this.isChecked = newValue;
+  }
+
+  @override
+  _IngredientTileState createState() =>
+      _IngredientTileState(title: title, changed: isChecked);
+}
+
+class _IngredientTileState extends State<IngredientTile> {
+  String title;
+  bool changed = false;
+
+  _IngredientTileState({required this.title, required this.changed});
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      value: changed,
+      onChanged: (bool? value) {
+        setState(() {
+          changed = value!;
+          super.widget.newValue(changed);
+        });
+      },
+      title: Text("$title"),
+    );
+  }
+}
